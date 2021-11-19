0c52ea2fe10c86492bc511e45cc569473d4709c1
RA <75052820+arraayyy@users.noreply.github.com>
2021-11-19 22:20:31 +0800

    Create monsterBeast.java

---
+++ Adv Folder/monsterBeast.java
@@ -0,0 +1,12 @@
+package package02;
+
+public class monsterBeast extends Monster{
+
+    public monsterBeast() {
+
+        name = "Goblin";
+        hp = 50;
+        attack = 20;
+        atkMes = "You took a punch from the beast";
+    }
+}
