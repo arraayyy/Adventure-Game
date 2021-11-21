35e14a8294817b93b3b99e512d37c6615f90b1b8
RA <75052820+arraayyy@users.noreply.github.com>
2021-11-21 14:47:21 +0800

    Update monsterGoblin.java

--- Adv Folder/monsterGoblin.java
+++ Adv Folder/monsterGoblin.java
@@ -4,14 +4,13 @@ package package02;
 public class monsterGoblin extends Monster {
 
     public monsterGoblin() {
-        name = "Beast";
+
+        name = "Goblin";
         hp = 20;
         attack = 6;
-        atkMes = "The goblin hit you with a club!";
+        atkMes = "The goblin hit you with a club!\n\n";
 
     }
-
-
 }
 
 
