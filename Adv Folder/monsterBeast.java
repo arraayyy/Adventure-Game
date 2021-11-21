4350d644d948816ed4dec12e12902c4d2281392f
RA <75052820+arraayyy@users.noreply.github.com>
2021-11-21 14:46:54 +0800

    Update monsterBeast.java

--- Adv Folder/monsterBeast.java
+++ Adv Folder/monsterBeast.java
@@ -4,9 +4,9 @@ public class monsterBeast extends Monster{
 
     public monsterBeast() {
 
-        name = "Goblin";
+        name = "Beast";
         hp = 50;
         attack = 20;
-        atkMes = "You took a punch from the beast";
+        atkMes = "You took a punch from the beast\n\n";
     }
 }
